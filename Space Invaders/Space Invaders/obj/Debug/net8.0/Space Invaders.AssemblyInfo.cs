0902ba6e30ae4afb2ccec7c51ed103e3a8885dc9
Jacob Schaub <32137916+schaub-power@users.noreply.github.com>
2025-06-02 12:45:19 +0200

    Add files via upload

--- Space Invaders/Space Invaders/obj/Debug/net8.0/Space Invaders.AssemblyInfo.cs
+++ Space Invaders/Space Invaders/obj/Debug/net8.0/Space Invaders.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Space Invaders")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6eaf0d3b0460d950981137cc2a2432d952f69c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Space Invaders")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Space Invaders")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
